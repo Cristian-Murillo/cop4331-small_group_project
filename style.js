415eccd5a8a3a620bb20596bac437e1505343d21
QuanminhN <86091771+QuanminhN@users.noreply.github.com>
2021-08-28 00:35:04 -0400

    basic HTML
    made basic HTML, css, js

---
+++ style.js
@@ -0,0 +1,6 @@
+function saveInfo() {
+    let userInfo = document.getElementById("user").value;
+    let passInfo = document.getElementById("pass").value;
+    document.getElementById("user").value = "";
+    document.getElementById("pass").value = "";
+}
