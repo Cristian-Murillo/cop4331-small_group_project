8101b0a291cd522559547e72e36b34b28f929fb3
Grayson-North <54782867+Grayson-North@users.noreply.github.com>
2021-09-03 16:00:09 -0400

    Add files via upload
    Add contact API endpoint

---
+++ LAMPAPI/Add.php
@@ -0,0 +1,46 @@
+<?php
+	ini_set('display_errors', 'On');
+	ini_set("error_log", "/tmp/php.log");
+	error_reporting(E_ALL);
+
+	$inData = getRequestInfo();
+
+	$userId = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$email = $inData["email"];
+	$phoneNumber = $inData["phoneNumber"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "ContactManager");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+ 	{
+		$stmt = $conn->prepare("INSERT into Contacts (userID,firstName,lastName,email,phoneNumber) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssssd", $userId, $firstName, $lastName, $email, $phoneNumber);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
